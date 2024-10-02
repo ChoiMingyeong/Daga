adc8d897db30159ee92d1a4891bace376ee3a08a
최민경 <alsruddl1119@naver.com>
2024-10-02 16:42:39 +0900

    Peer 클래스 생성

---
+++ Common/DagaDev/Peer.cs
@@ -0,0 +1,20 @@
+﻿namespace DagaDev
+{
+    public class Peer
+    {
+        public bool Connect(string ipAddress, int port)
+        {
+            return true;
+        }
+
+        public void Listen(int port)
+        {
+
+        }
+
+        public void Disconnect()
+        {
+
+        }
+    }
+}
