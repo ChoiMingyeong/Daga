dae317323374ee7d79e4cfe6f84539c9a21300fc
min <alsruddl1119@naver.com>
2024-10-19 23:13:42 +0900

    Account Sheet 테스트용 post

--- Common/DagaDev/Program.cs
+++ Common/DagaDev/Program.cs
@@ -1,13 +1,28 @@
-﻿namespace DagaNetwork
+﻿using System.Text;
+
+namespace DagaNetwork
 {
     internal class Program
     {
         static async Task Main(string[] args)
         {
-            HttpClient httpClient = new HttpClient();
-            Uri uri = new Uri();
+            string url = "https://script.google.com/macros/s/AKfycbxVBoEEgN7E11tNwN3t_zKSrxULh6zo60hLoQ_oM5cK6waOFUhOwTKES8Ad4Co4si5l7g/exec";
+            string jsonData = "{{\"action\": 1,\"id\":\"test1\", \"pwd\":\"1111\", \"nickname\":\"test\"}}";
+            HttpClient httpClient = new();
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
 
-            await httpClient.PostAsync();
+            try
+            {
+                var response = await httpClient.PostAsync(url, content);
+                response.EnsureSuccessStatusCode(); // 응답 코드가 성공(200~299) 범위인지 확인
+
+                var responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine($"서버 응답: {responseBody}");
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"요청 오류: {e.Message}");
+            }
         }
     }
 }
