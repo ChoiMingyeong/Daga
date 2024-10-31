0ed1bd8f8dfc63ce992881244b7d2c8bedb03ef6
최민경 <alsruddl1119@naver.com>
2024-10-31 14:22:42 +0900

    통합 소스 제너레이터 생성

---
+++ Common/DagaSourceGenerator/SourceGenerator.cs
@@ -0,0 +1,9 @@
+﻿namespace DagaSourceGenerator
+{
+    public class SourceGenerator
+    {
+        private Dictionary<Namespace, string> _sheets = [];
+
+        public bool CombineCode { get; set; } = false;
+    }
+}
