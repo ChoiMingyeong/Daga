a898bec9053311698c0af3fe4b59a561f1e319dd
최민경 <alsruddl1119@naver.com>
2024-10-31 14:32:54 +0900

    통합 소스 제너레이터 옵션 별도로 구분

--- Common/DagaSourceGenerator/SourceGenerator.cs
+++ Common/DagaSourceGenerator/SourceGenerator.cs
@@ -4,6 +4,28 @@
     {
         private Dictionary<Namespace, string> _sheets = [];
 
-        public bool CombineCode { get; set; } = false;
+
+        private SourceGeneratorOption? _option;
+        public SourceGeneratorOption Option
+        {
+            get
+            {
+                _option ??= new();
+                return _option;
+            }
+
+            init
+            {
+                _option = value;
+            }
+        }
+    }
+
+    public class SourceGeneratorOption
+    {
+        /// <summary>
+        /// 동일한 네임스페이스 코드 소스 병합 여부
+        /// </summary>
+        public bool CombineCode { get; init; } = false;
     }
 }
