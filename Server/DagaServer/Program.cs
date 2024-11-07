ecee58853df3dd77d3db3032da7aec5a60a18d1e
최민경 <alsruddl1119@naver.com>
2024-11-07 14:32:05 +0900

    ASP.Core 웹 API 서버로 서버 프로젝트 재생성

--- Server/DagaServer/Program.cs
+++ Server/DagaServer/Program.cs
@@ -1,10 +1,53 @@
-﻿namespace DagaServer
+
+namespace DagaServer
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddSingleton<ConnectionService>();
+
+            var app = builder.Build();
+
+            app.UseWebSockets();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Map("/ws", async (HttpContext context) =>
+            {
+                if(context.WebSockets.IsWebSocketRequest)
+                {
+                    
+                }
+                else
+                {
+                    context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                }
+            });
+
+            app.Run();
         }
     }
 }
