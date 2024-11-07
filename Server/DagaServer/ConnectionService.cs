8913b7e6006a879e3f26bf70bd0c6a8786bde303
최민경 <alsruddl1119@naver.com>
2024-11-07 14:32:47 +0900

    의존성 주입 Singleton 객체로 ConnectionService 추가

---
+++ Server/DagaServer/ConnectionService.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.WebSockets;
+
+namespace DagaServer
+{
+    public class ConnectionService
+    {
+        private Dictionary<long, WebSocket> _clients = [];
+
+        public ConnectionService()
+        {
+
+        }
+
+
+    }
+}
