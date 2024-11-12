bbb205cb15a20d42b00b98f65860c08173f74a54
최민경 <alsruddl1119@naver.com>
2024-11-12 17:14:58 +0900

    chat controller 추가

---
+++ Server/DagaServer/Controllers/ChatController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace DagaServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ChatController : ControllerBase
+    {
+        public ChatController()
+        {
+
+        }
+
+        [HttpPost(Name = "chat")]
+        public string Post(string msg)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            return stringBuilder.ToString();
+        }
+    }
+}
