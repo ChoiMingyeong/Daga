711c64da908c66fe68828f5f538db1edb97279fa
최민경 <alsruddl1119@naver.com>
2024-11-12 17:14:24 +0900

    csv 리더 추가

---
+++ Common/DagaSourceGenerator/CSVReader.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace DagaSourceGenerator
+{
+    public static class CSVReader
+    {
+        public static List<List<string>> CSVSeperator(string filePath)
+        {
+            Debug.Assert(false == string.IsNullOrEmpty(filePath));
+            Debug.Assert(Directory.Exists(filePath));
+
+            List<List<string>> result = [];
+
+            return result;
+        }
+    }
+}
