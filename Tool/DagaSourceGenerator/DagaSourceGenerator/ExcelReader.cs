92402864ef8bbb27a89be06182ea566f9c01dab6
최민경 <alsruddl1119@naver.com>
2024-11-13 11:39:03 +0900

    파일 ExcelReader 클래스 생성

---
+++ Tool/DagaSourceGenerator/DagaSourceGenerator/ExcelReader.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace DagaSourceGenerator
+{
+    public static class ExcelReader
+    {
+        private static readonly string _pattern = @"^(?!~\$).*\.(xlsx|csv)$";
+
+        private static readonly Regex _regex = new(_pattern, RegexOptions.IgnoreCase);
+
+        private static bool CanRead(string fileName)
+        {
+            return _regex.IsMatch(fileName);
+        }
+
+        public static string[] GetReadableFilePaths(string folderPath)
+        {
+            Debug.Assert(Directory.Exists(folderPath));
+
+            return Directory.GetFiles(folderPath).Where(p=>CanRead(Path.GetFileName(p))).ToArray();
+        }
+
+
+    }
+}
