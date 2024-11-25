22d1f50514d7c2c825f500d5a8642daa7dfa3bf1
최민경 <alsruddl1119@naver.com>
2024-11-25 14:48:49 +0900

    Roslyn 으로 코드 생성 추가

--- Tool/DagaCodeGenerator/Code/EnumCode.cs
+++ Tool/DagaCodeGenerator/Code/EnumCode.cs
@@ -1,9 +1,33 @@
-﻿namespace DagaCodeGenerator.Code;
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System.Diagnostics;
+using System.Text;
+
+namespace DagaCodeGenerator.Code;
 
 public class EnumCode : ICode
 {
     public override bool CreateFile()
     {
         throw new NotImplementedException();
+    }
+
+    public override string ToString()
+    {
+        Debug.Assert(null != _namespaceDeclaration);
+        return _namespaceDeclaration.NormalizeWhitespace().ToFullString();
+    }
+
+    protected override void InitDeclarations(in IEnumerable<string[]> readLines)
+    {
+        _namespaceDeclaration = SyntaxFactory.NamespaceDeclaration(SyntaxFactory.ParseName(Namespace))
+            .WithNamespaceKeyword(SyntaxFactory.Token(SyntaxKind.NamespaceKeyword))
+            .NormalizeWhitespace();
+
+        _classDeclaration = SyntaxFactory.ClassDeclaration(ClassName)
+            .WithModifiers(SyntaxFactory.TokenList(SyntaxFactory.Token(SyntaxKind.PublicKeyword), SyntaxFactory.Token(SyntaxKind.StaticKeyword)))
+            .NormalizeWhitespace();
+
+        _namespaceDeclaration = _namespaceDeclaration.AddMembers(_classDeclaration);
     }
 }
--- Tool/DagaCodeGenerator/Code/ICode.cs
+++ Tool/DagaCodeGenerator/Code/ICode.cs
@@ -1,8 +1,26 @@
-﻿namespace DagaCodeGenerator.Code;
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Text;
+
+namespace DagaCodeGenerator.Code;
 
 public abstract class ICode
 {
-    public required IEnumerable<string[]> ReadLines { get; init; }
+    public required string Namespace { get; init; }
+
+    public required string ClassName { get; init; }
+
+    public required IEnumerable<string[]> ReadLines
+    {
+        init
+        {
+            InitDeclarations(value);
+        }
+    }
+
+    protected NamespaceDeclarationSyntax? _namespaceDeclaration;
+    protected ClassDeclarationSyntax? _classDeclaration;
 
     public abstract bool CreateFile();
+
+    protected abstract void InitDeclarations(in IEnumerable<string[]> readLines);
 }
