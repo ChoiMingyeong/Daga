0eedfaf22c476b7ad35036cff3aef16565a34137
최민경 <alsruddl1119@naver.com>
2024-12-02 15:40:20 +0900

    로그인 API 컨트롤러 추가

---
+++ Editor/DagaEditor/LoginAPIController.cs
@@ -0,0 +1,44 @@
+﻿
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace DagaEditor
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginAPIController : ControllerBase
+    {
+        // GET: api/<LoginAPIController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<LoginAPIController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<LoginAPIController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<LoginAPIController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<LoginAPIController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
