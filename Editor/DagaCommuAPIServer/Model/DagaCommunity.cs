4531ae368b50ffec8f07cb9d9a248b23b0c169a9
최민경 <alsruddl1119@naver.com>
2024-12-06 13:52:32 +0900

    커뮤니티 클래스 생성

---
+++ Editor/DagaCommuAPIServer/Model/DagaCommunity.cs
@@ -0,0 +1,9 @@
+﻿namespace DagaCommuAPIServer.Model
+{
+    public class DagaCommunity(ulong communityID, byte dbIndex)
+    {
+        public readonly ulong CommunityID = communityID;
+
+        private readonly byte DBIndex = dbIndex;
+    }
+}
