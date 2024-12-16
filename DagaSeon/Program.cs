b0f66eca130f79065e725aa98ac9556e424eb968
최민경 <alsruddl1119@naver.com>
2024-12-16 12:34:33 +0900

    로그인 페이지 테스트

--- DagaSeon/Program.cs
+++ DagaSeon/Program.cs
@@ -11,22 +11,23 @@ namespace DagaSeon
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            //builder.Services.AddEndpointsApiExplorer();
+            //builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            //// Configure the HTTP request pipeline.
+            //if (app.Environment.IsDevelopment())
+            //{
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI();
+            //}
 
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
 
+            app.UseRouting();
 
             app.MapControllers();
 
