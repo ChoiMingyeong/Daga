e92410c8c0aed55ad4bfa1f45769a98911cd9dd0
최민경 <alsruddl1119@naver.com>
2024-12-23 15:55:09 +0900

    worker 서비스에서 signalR로 명령 받기

--- DagaRemote/DagaRemote/Worker.cs
+++ DagaRemote/DagaRemote/Worker.cs
@@ -1,18 +1,51 @@
+using DagaRemote.Hub;
+using Microsoft.AspNetCore.SignalR.Client;
+using System.Diagnostics;
+
 namespace DagaRemote
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private HubConnection _hubConnection;
 
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
+
+            _hubConnection = new HubConnectionBuilder()
+                .WithUrl("http://dagaops.duckdns.org:5001/remote")
+                .Build();
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            await _hubConnection.StartAsync();
+            _logger.LogInformation("SignalR Started.");
+
+            _hubConnection.On<string>("ReceiveMessage", message =>
+            {
+                _logger.LogInformation("Received message: {Message}", message);
+            });
+
+            _hubConnection.On<string>("RemoteCommand", command =>
+            {
+                if (false == string.IsNullOrWhiteSpace(command))
+                {
+                    _logger.LogInformation($"Remote Command : {command}");
+                    Process.Start(new ProcessStartInfo
+                    {
+                        Arguments = "",
+                        UseShellExecute = true,
+                        FileName = "",
+                    });
+                }
+            });
+
             while (!stoppingToken.IsCancellationRequested)
             {
+                await _hubConnection.SendAsync("SendMessage", "Hello from Worker Service");
+
                 if (_logger.IsEnabled(LogLevel.Information))
                 {
                     _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
