24ce89bfefd111aac16da976482f8c1cfc7f040c
ChoiMingyeong <alsruddl1119@naver.com>
2024-12-29 22:27:28 +0900

    RemoteController.cs 업데이트

--- DagaRemote/DagaRemote/RemoteController.cs
+++ DagaRemote/DagaRemote/RemoteController.cs
@@ -4,10 +4,16 @@ using DagaUtility;
 public class RemoteController : Singleton<RemoteController>
 {
     public RemoteOption RemoteOption { get; set; } = new();
+    private Dictionary<int, Process> _detachProcesses=[];
 
     public bool StartProcess(ProcessType processType, Version version)
     {
 
+        return true;
+    }
+
+    public bool StopProcess(Version version, pararms int processID)
+    {
         return true;
     }
 }
