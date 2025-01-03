3b31b5ae654eeaf099aa8102e482cf81214b4417
최민경 <alsruddl1119@naver.com>
2025-01-03 18:39:15 +0900

    누락 코드 추가

--- DagaDataGenerator/DagaDataGenerator/SrcGenerator/Packet/PacketSrcGenerator.cs
+++ DagaDataGenerator/DagaDataGenerator/SrcGenerator/Packet/PacketSrcGenerator.cs
@@ -2,17 +2,16 @@ namespace DagaDataGenerator.SrcGenerator.Packet;
 
 public class PacketSrcGenerator : ISrcGenerator
 {
-    public bool AddEntity()
-    {
-        throw new NotImplementedException();
-    }
     public bool CreateSource()
     {
         throw new NotImplementedException();
     }
-
     public void Dispose()
     {
         GC.SuppressFinalize(this);
+    }
+    public bool TryAddEntity(params object?[]? objects)
+    {
+        throw new NotImplementedException();
     }
 }
