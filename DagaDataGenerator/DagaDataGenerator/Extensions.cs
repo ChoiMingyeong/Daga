c5ce864cd4c3126d7986173bc5712084c95cda44
최민경 <alsruddl1119@naver.com>
2025-01-15 15:34:49 +0900

    Extension 인라인 함수 추가
    - [MethodImpl(MethodImplOptions.AggressiveInlining)] 키워드 추가로 인라인 함수 추가

--- DagaDataGenerator/DagaDataGenerator/Extensions.cs
+++ DagaDataGenerator/DagaDataGenerator/Extensions.cs
@@ -1,16 +1,20 @@
 ﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Runtime.CompilerServices;
+using System.Security.AccessControl;
 
 namespace DagaDataGenerator
 {
     internal static class Extensions
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static SyntaxTrivia CreateComment(string comment)
         {
             return SyntaxFactory.Comment($"// {comment}");
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static SyntaxTriviaList CreateSummary(string summary)
         {
             return SyntaxFactory.TriviaList(
@@ -34,21 +38,25 @@ namespace DagaDataGenerator
             );
         }
 
-        internal static T AddLeadingComment<T>(T memberDeclaration, string comment) where T : MemberDeclarationSyntax
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static T AddLeadingComment<T>(ref T memberDeclaration, string comment) where T : MemberDeclarationSyntax
         {
             return memberDeclaration.WithLeadingTrivia(CreateComment(comment));
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static T AddTrailingComment<T>(T memberDeclaration, string comment) where T : MemberDeclarationSyntax
         {
             return memberDeclaration.WithTrailingTrivia(CreateComment(comment));
         }
 
-        internal static T AddSummary<T>(T memberDeclaration, string summary) where T : MemberDeclarationSyntax
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static T AddSummary<T>(ref T memberDeclaration, string summary) where T : MemberDeclarationSyntax
         {
             return memberDeclaration.WithLeadingTrivia(CreateSummary(summary));
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static NamespaceDeclarationSyntax CreateNamespace(string @namespace)
         {
             return SyntaxFactory.NamespaceDeclaration(SyntaxFactory.ParseName(@namespace));
