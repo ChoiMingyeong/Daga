01ec3a62e9134ba406c3ecd9a990a38684934593
최민경 <alsruddl1119@naver.com>
2025-01-17 14:23:28 +0900

    IDataTable -> DataTableEntity로 요소 이동

--- DagaDataGenerator/DagaDataGenerator/SrcGenerator/DataTable/DataTableEntity.cs
+++ DagaDataGenerator/DagaDataGenerator/SrcGenerator/DataTable/DataTableEntity.cs
@@ -1,7 +1,9 @@
-﻿namespace DagaDataGenerator.SrcGenerator.DataTable
+﻿using System.Data;
+
+namespace DagaDataGenerator.SrcGenerator.DataTable
 {
     public class DataTableEntity : IEntity
     {
-
+        public readonly uint DataTableID;
     }
 }
--- DagaDataGenerator/DagaDataGenerator/SrcGenerator/DataTable/IDataTable.cs
+++ DagaDataGenerator/DagaDataGenerator/SrcGenerator/DataTable/IDataTable.cs
@@ -1,7 +1,8 @@
 ﻿namespace DagaDataGenerator.SrcGenerator.DataTable;
 
-public class IDataTable(uint dataTableID, string name, string? summary = null, params DataTableEntity[] entities)
+public class IDataTable(string name, string? summary = null, params DataTableEntity[] entities)
     : ISrc<DataTableEntity>(name, summary, entities)
 {
     public readonly uint DataTableID = dataTableID;
+
 }
