cfe6aad6ca46c1c4d531593d95e0f74c0c52af94
최민경 <alsruddl1119@naver.com>
2025-01-17 14:25:59 +0900

    누락 코드 추가

--- DagaDataGenerator/DagaDataGenerator/SrcGenerator/DataTable/IDataTable.cs
+++ DagaDataGenerator/DagaDataGenerator/SrcGenerator/DataTable/IDataTable.cs
@@ -3,6 +3,5 @@
 public class IDataTable(string name, string? summary = null, params DataTableEntity[] entities)
     : ISrc<DataTableEntity>(name, summary, entities)
 {
-    public readonly uint DataTableID = dataTableID;
 
 }
