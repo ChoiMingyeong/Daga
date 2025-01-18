b6ee7c883b8072391d2f68a8809eb09f3845c802
ChoiMingyeong <alsruddl1119@naver.com>
2025-01-18 23:12:24 +0900

    IDataTable.cs 업데이트

--- DagaDataGenerator/DagaDataGenerator/SrcGenerator/DataTable/IDataTable.cs
+++ DagaDataGenerator/DagaDataGenerator/SrcGenerator/DataTable/IDataTable.cs
@@ -3,5 +3,8 @@
 public class IDataTable(string name, string? summary = null, params DataTableEntity[] entities)
     : ISrc<DataTableEntity>(name, summary, entities)
 {
-
+    public bool TryAddEntities(string typeName, string name, string value)
+    {
+        return true;
+    }
 }
