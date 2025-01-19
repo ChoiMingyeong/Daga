1ac257ab0ecec4ee352e84e5a2c23e9d17b93e81
min <alsruddl1119@naver.com>
2025-01-19 23:51:16 +0900

    데이터테이블 ID 생성

--- DagaDataGenerator/DagaDataGenerator/SrcGenerator/DataTable/DataTableEntity.cs
+++ DagaDataGenerator/DagaDataGenerator/SrcGenerator/DataTable/DataTableEntity.cs
@@ -5,5 +5,10 @@ namespace DagaDataGenerator.SrcGenerator.DataTable
     public class DataTableEntity : IEntity
     {
         public readonly uint DataTableID;
+
+        public DataTableEntity(uint tableID)
+        {
+            DataTableID = tableID;
+        }
     }
 }
--- DagaDataGenerator/DagaDataGenerator/SrcGenerator/DataTable/IDataTable.cs
+++ DagaDataGenerator/DagaDataGenerator/SrcGenerator/DataTable/IDataTable.cs
@@ -3,8 +3,16 @@
 public class IDataTable(string name, string? summary = null, params DataTableEntity[] entities)
     : ISrc<DataTableEntity>(name, summary, entities)
 {
+    private uint _createID = 1;
+    public uint CreateID { get
+        {
+            return Interlocked.Increment(ref _createID);
+        } 
+    }
     public bool TryAddEntities(string typeName, string name, string value)
     {
+
+
         return true;
     }
 }
