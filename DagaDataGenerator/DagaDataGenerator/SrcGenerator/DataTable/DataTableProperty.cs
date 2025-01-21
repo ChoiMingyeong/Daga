bdef7af3afdfcc527e85d2d6dbf4baef52a6bd8b
최민경 <alsruddl1119@naver.com>
2025-01-21 14:23:34 +0900

    DataTalbeEntity->DataTableProperty로 이름 변경

---
+++ DagaDataGenerator/DagaDataGenerator/SrcGenerator/DataTable/DataTableProperty.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace DagaDataGenerator.SrcGenerator.DataTable
+{
+    public class DataTableProperty : IEntity
+    {
+        public readonly uint Index;
+
+        public required string TypeName { get; set; }
+
+        public required string Name { get; set; }
+
+        public string? Comment { get; set; }
+
+        public DataTableProperty(uint index)
+        {
+            Index = index;
+        }
+    }
+}
