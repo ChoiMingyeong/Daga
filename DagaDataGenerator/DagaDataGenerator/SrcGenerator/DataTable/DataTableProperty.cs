c35ad453ee93dacd5fb91e7e9b6aa7e1dca0634f
최민경 <alsruddl1119@naver.com>
2025-01-22 16:23:33 +0900

    DataTable 업데이트

--- DagaDataGenerator/DagaDataGenerator/SrcGenerator/DataTable/DataTableProperty.cs
+++ DagaDataGenerator/DagaDataGenerator/SrcGenerator/DataTable/DataTableProperty.cs
@@ -1,20 +1,25 @@
-﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace DagaDataGenerator.SrcGenerator.DataTable
 {
     public class DataTableProperty : IEntity
     {
-        public readonly uint Index;
-
         public required string TypeName { get; set; }
 
         public required string Name { get; set; }
 
         public string? Comment { get; set; }
 
-        public DataTableProperty(uint index)
+        public DataTableProperty()
         {
-            Index = index;
+        }
+
+        public PropertyDeclarationSyntax ToSource()
+        {
+            var declaration = SyntaxFactory.PropertyDeclaration();
+
+            return declaration;
         }
     }
 }
--- DagaDataGenerator/DagaDataGenerator/SrcGenerator/DataTable/IDataTable.cs
+++ DagaDataGenerator/DagaDataGenerator/SrcGenerator/DataTable/IDataTable.cs
@@ -28,7 +28,9 @@ public class IDataTable(string name, string? summary = null, params DataTablePro
 
     public ClassDeclarationSyntax ToSource()
     {
-        var declaration = SyntaxFactory.ClassDeclaration();
+        var declaration = SyntaxFactory.ClassDeclaration(Name)
+            .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword))
+            .AddMembers(Entities.Select(p=>p.ToSource()).ToArray());
 
         return declaration;
     }
