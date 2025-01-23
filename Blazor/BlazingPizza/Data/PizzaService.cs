0a050ef230c73bdf320d1fc8fc5e40abbfa50059
최민경 <alsruddl1119@naver.com>
2025-01-23 14:55:12 +0900

    BlazingPizza 데이터 액세스까지

---
+++ Blazor/BlazingPizza/Data/PizzaService.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazingPizza.Data
+{
+    public class PizzaService
+    {
+        public Task<Pizza[]> GetPizzasAsync()
+        {
+            return Task.FromResult(Array.Empty<Pizza>());
+        }
+    }
+}
