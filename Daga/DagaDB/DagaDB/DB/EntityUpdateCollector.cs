38ac96a5d090cf79682b35d4efc74ba983ca0cc6
최민경 <alsruddl1119@naver.com>
2025-03-18 17:16:50 +0900

    주석 추가해보기

--- Daga/DagaDB/DagaDB/DB/EntityUpdateCollector.cs
+++ Daga/DagaDB/DagaDB/DB/EntityUpdateCollector.cs
@@ -25,6 +25,10 @@ namespace DagaDB.DB
 
     public static class CalculatorHelper
     {
+        /// <summary>
+        /// <see cref="typeof(T)"/>가 숫자타입일 때, target의 값을 source의 값과 합산합니다.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">target이나 source가 null이면 발생</exception>
         public static void AddProperties<T>(T target, T source)
         {
             if (target == null || source == null)
