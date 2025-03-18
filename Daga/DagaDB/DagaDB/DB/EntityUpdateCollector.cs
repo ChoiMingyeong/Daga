b10158ab93706f8bd319ff31381d52944c5c8bd1
최민경 <alsruddl1119@naver.com>
2025-03-18 18:36:21 +0900

    GetKey() 최적화
    - 키 캐싱 추가로 키 속성 값을 가져오는 리플렉션 호출을 최소화하여 성능 향상

--- Daga/DagaDB/DagaDB/DB/EntityUpdateCollector.cs
+++ Daga/DagaDB/DagaDB/DB/EntityUpdateCollector.cs
@@ -1,6 +1,7 @@
 ﻿using DagaCommon;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Collections.Concurrent;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Reflection;
@@ -31,15 +32,18 @@ namespace DagaDB.DB
     {
         public readonly Dictionary<object, TestTable> Entities = [];
 
+        private static readonly ConcurrentDictionary<Type, PropertyInfo[]> _keyPropertyCache = new();
+
         public static object? GetKey<T>(T entity)
         {
             if (entity == null)
                 throw new ArgumentNullException(nameof(entity));
 
             var type = typeof(T);
-            var keyProperties = type.GetProperties()
-                                    .Where(p => p.GetCustomAttribute<KeyAttribute>() != null)
-                                    .ToArray();
+            var keyProperties = _keyPropertyCache.GetOrAdd(type, t =>
+                t.GetProperties()
+                 .Where(p => p.GetCustomAttribute<KeyAttribute>() != null)
+                 .ToArray());
 
             if (keyProperties.Length == 0)
                 throw new InvalidOperationException($"클래스 {type.Name}에 [Key] 속성이 없습니다.");
