a79f3bd3bdb03a3b269fbd3fefa2e76d6c3f96b3
최민경 <alsruddl1119@naver.com>
2025-03-19 11:43:01 +0900

    TestTable.cs 형식 이동

---
+++ Daga/DagaDB/DagaDB/DB/TestTable.cs
@@ -0,0 +1,26 @@
+﻿using DagaCommon;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DagaDB.DB
+{
+    [Table("test")]
+    public class TestTable
+    {
+        [Key]
+        [Column("id")]
+        public uint Id { get; set; }
+
+        [Calculate]
+        [Column("value")]
+        public int Value { get; set; }
+
+        [Column("value2")]
+        public int Value2 { get; set; } = 0;
+
+        [Calculate]
+        [Column("value3")]
+        public string Value3 { get; set; } = string.Empty;
+    }
+}
