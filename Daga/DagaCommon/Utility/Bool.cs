c91a69ceea71dfbbc53578241b42b5847576575e
최민경 <alsruddl1119@naver.com>
2025-03-20 19:07:13 +0900

    byte로 bool 8개 관리하는 Bool 클래스 생성

---
+++ Daga/DagaCommon/Utility/Bool.cs
@@ -0,0 +1,70 @@
+﻿namespace DagaCommon.Utility
+{
+    public class Bool
+    {
+        private static readonly byte[] _flags = [1, 2, 4, 8, 16, 32, 64, 128];
+
+        private byte _value = 0;
+
+        public bool this[byte index]
+        {
+            get
+            {
+                ValidateIndex(index);
+                return (_value & _flags[index]) != 0;
+            }
+            set
+            {
+                ValidateIndex(index);
+                if(value)
+                {
+                    _value |= _flags[index];
+                }
+                else
+                {
+                    _value &= (byte)~_flags[index];
+                }
+            }
+        }
+
+        public Bool(IEnumerable<bool> boolList)
+        {
+            var list = boolList.Take(8).ToList(); // 최대 8개만 가져와 리스트로 변환
+            byte length = (byte)list.Count;
+
+            for (byte i = 0; i < length; i++)
+            {
+                this[i] = list[i];
+            }
+        }
+
+        public Bool(params bool[] boolList) : this((IEnumerable<bool>)boolList)
+        {
+        }
+
+        public IEnumerable<bool> ToEnumerable()
+        {
+            for (byte i = 0; i < _flags.Length; i++)
+            {
+                yield return (_value & _flags[i]) != 0;
+            }
+        }
+
+        public List<bool> ToList()
+        {
+            return ToEnumerable().ToList();
+        }
+
+        public bool[] ToArray()
+        {
+            return ToEnumerable().ToArray();
+        }
+        private void ValidateIndex(byte index)
+        {
+            if (index >= 8)
+            {
+                throw new IndexOutOfRangeException("Index must be between 0 and 7.");
+            }
+        }
+    }
+}
