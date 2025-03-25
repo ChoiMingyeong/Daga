586b0e3628b616ac5d36e85f317bbb7d664ce530
최민경 <alsruddl1119@naver.com>
2025-03-25 16:26:57 +0900

    RoleController 추가

---
+++ Daga/DagaDB/DagaDB/Controllers/RoleController.cs
@@ -0,0 +1,40 @@
+﻿using DagaCommon.Models;
+using DagaCommon.Protocol;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DagaDB.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class RoleController : ControllerBase
+{
+    private Dictionary<ulong, List<Role>> _roles = [];
+
+    public RoleController()
+    {
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostAsync([FromBody] RequestCreateRole recPacket)
+    {
+
+
+        return Ok();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAsync([FromQuery] ulong projectID)
+    {
+
+
+        return Ok();
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Async([FromBody] Role data)
+    {
+
+
+        return Ok();
+    }
+}
