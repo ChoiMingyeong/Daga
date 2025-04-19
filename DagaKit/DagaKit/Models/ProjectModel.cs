39e81d91bf7ae3812481af39fb6bc2db23cc9c24
min <alsruddl1119@naver.com>
2025-04-20 05:33:46 +0900

    DagaKit 솔루션 생성

---
+++ DagaKit/DagaKit/Models/ProjectModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DagaKit.Models
+{
+    public class ProjectModel
+    {
+        [Required]
+        public ulong Id { get; init; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+    }
+}
