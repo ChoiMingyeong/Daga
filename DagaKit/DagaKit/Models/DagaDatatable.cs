e402c3fca818e2c99ba31821eb6e050371564d4c
min <alsruddl1119@naver.com>
2025-04-21 03:26:59 +0900

    데이터테이블 편집 페이지 추가

---
+++ DagaKit/DagaKit/Models/DagaDatatable.cs
@@ -0,0 +1,23 @@
+﻿using System.Data;
+
+namespace DagaKit.Models
+{
+    public class DagaDatatable : DataTable
+    {
+        public List<DataColumn> DataColumns => [.. Columns.Cast<DataColumn>()];
+
+        public ushort Id { get; init; }
+
+        public DagaDatatable(string tableName) : base(tableName)
+        {
+            DataColumn idColumn = new("Id", typeof(uint))
+            {
+                AutoIncrement = true,
+                AutoIncrementSeed = 1,
+                AutoIncrementStep = 1,
+                ReadOnly = true
+            };
+            Columns.Add(idColumn);
+        }
+    }
+}
