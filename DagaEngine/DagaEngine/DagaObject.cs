3b3b8de6aa78604d83f875159d29d1eacccf9dbe
최민경 <alsruddl1119@naver.com>
2025-05-12 14:38:44 +0900

    DagaObject 기본 함수 추가

--- DagaEngine/DagaEngine/DagaObject.cs
+++ DagaEngine/DagaEngine/DagaObject.cs
@@ -1,7 +1,29 @@
-﻿namespace DagaEngine
+﻿using TSID.Creator.NET;
+
+namespace DagaEngine
 {
+
     public class DagaObject
     {
+        public DagaObject()
+        {
+            
+        }
 
+        public Tsid ObjectID { get; init; } = TsidCreator.GetTsid();
+
+        public string Name { get; set; } = string.Empty;
+
+        public async Task InitializeAsync()
+        {
+            // Simulate some work
+            await Task.Delay(100);
+        }
+
+        public async Task UpdateAsync()
+        {
+            // Simulate some work
+            await Task.Delay(100);
+        }
     }
 }
