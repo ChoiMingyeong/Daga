b76af5a48c25a603cc29cfd60eb119794091e74c
최민경 <alsruddl1119@naver.com>
2025-05-12 14:39:17 +0900

    DagaObjectManager 생성

---
+++ DagaEngine/DagaEngine/DagaObjectManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Concurrent;
+using TSID.Creator.NET;
+
+namespace DagaEngine
+{
+    public class DagaObjectManager : DagaManager
+    {
+        private ConcurrentDictionary<Tsid, DagaObject> objects = [];
+
+        public DagaObjectManager()
+        {
+
+        }
+
+        public sealed override async Task InitializeAsync()
+        {
+            await Parallel.ForEachAsync(objects.Values, async (obj, _) =>
+            {
+                await obj.InitializeAsync();
+            });
+        }
+
+        public sealed override async Task UpdateAsync()
+        {
+            await Parallel.ForEachAsync(objects.Values, async (obj, _) =>
+            {
+                await obj.UpdateAsync();
+            });
+        }
+    }
+}
