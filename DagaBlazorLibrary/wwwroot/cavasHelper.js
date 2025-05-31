397168ebd40e6ec89d1fc9195a633ea442d9fb1c
min <alsruddl1119@naver.com>
2025-05-31 23:22:48 +0900

    사각형 그리는 canvasHelper.js 추가

---
+++ DagaBlazorLibrary/wwwroot/cavasHelper.js
@@ -0,0 +1,25 @@
+﻿window.canvasHelper = (function () {
+    let ctx = null;
+    let canvas = null;
+
+    return {
+        init: function (canvasId) {
+            canvas = document.getElementById(canvasId);
+            if (canvas && canvas.getContext) {
+                ctx = canvas.getContext('2d');
+            }
+        },
+        drawRect: function (x, y, width, height, fillColor, strokeColor) {
+            if (!ctx) return;
+            ctx.fillStyle = fillColor;
+            ctx.fillRect(x, y, width, height);
+            ctx.strokeStyle = strokeColor;
+            ctx.strokeRect(x, y, width, height);
+        },
+        clear: function () {
+            if (ctx && canvas) {
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+            }
+        }
+    };
+})();
