5157a78e66ede1452b5a19c84d0ddc6cadfa663e
최민경 <alsruddl1119@naver.com>
2025-06-02 15:32:03 +0900

    CanvasHelperService 추가

---
+++ DagaBlazorLibrary/Services/CanvasHelperService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.JSInterop;
+
+namespace DagaBlazorLibrary.Services
+{
+    public class CanvasHelperService
+    {
+        private readonly IJSRuntime _jsRuntime;
+
+        public CanvasHelperService(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+        }
+
+        public async Task InitAsync(string canvasId)
+        {
+            await _jsRuntime.InvokeVoidAsync("canvasHelper.init", canvasId);
+        }
+
+        public async Task DrawRectAsync(double x, double y, double width, double height, string fillColor, string strokeColor)
+        {
+            await _jsRuntime.InvokeVoidAsync("canvasHelper.drawRect", x, y, width, height, fillColor, strokeColor);
+        }
+        public async Task DrawImageAsync(string imageUrl, double x, double y, double width, double height)
+        {
+            await _jsRuntime.InvokeVoidAsync("canvasHelper.drawImage", imageUrl, x, y, width, height);
+        }
+
+        public async Task ClearAsync()
+        {
+            await _jsRuntime.InvokeVoidAsync("canvasHelper.clear");
+        }
+    }
+
+}
