32ab0c0f4c978d43456acb28c1fd89a2b6285d0c
최민경 <alsruddl1119@naver.com>
2025-06-02 15:32:23 +0900

    라이브러리 테스트용 솔루션 추가

---
+++ BlazorApp1/Program.cs
@@ -0,0 +1,22 @@
+using DagaBlazorLibrary.Services;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace BlazorApp1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<IScreenSizeService, ScreenSizeService>();
+            builder.Services.AddScoped<CanvasHelperService>();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
