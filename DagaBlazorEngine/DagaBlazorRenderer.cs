ce328fbf89dc2d1a533b7242626c1e5d36cb9058
최민경 <alsruddl1119@naver.com>
2025-06-04 13:55:06 +0900

    DagaBlazorEngine 새로 구성

---
+++ DagaBlazorEngine/DagaBlazorRenderer.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.JSInterop;
+
+namespace DagaBlazorEngine
+{
+    public class DagaBlazorRenderer
+    {
+        private readonly IJSRuntime _jsRuntime;
+
+        public DagaBlazorRenderer(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+        }
+    }
+}
