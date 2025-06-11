c9cd825eca871f524f3d8280585cf58aedfce4bf
최민경 <alsruddl1119@naver.com>
2025-06-11 17:36:08 +0900

    원형 그리기, text 입력, image 그리기 추가

--- DagaBlazorEngine/wwwroot/js/canvas.js
+++ DagaBlazorEngine/wwwroot/js/canvas.js
@@ -8,35 +8,82 @@ export function init(canvas) {
     targetCtx = canvas.getContext("2d");
     offscreen = new OffscreenCanvas(canvas.width, canvas.height);
     offscreenCtx = offscreen.getContext("2d");
+
+    resizeCanvas();
+    window.addEventListener("resize", resizeCanvas());
 }
 
-export function drawBegin() {
-    if (!offscreenCtx) return;
+function resizeCanvas() {
+    if (!targetCtx || !offscreen) return;
 
     const { w, h } = getScreenSize();
     targetCtx.canvas.width = w;
     targetCtx.canvas.height = h;
     offscreen.width = w;
     offscreen.height = h;
+}
+
+export function drawBegin() {
+    if (!offscreenCtx) return;
 
     offscreenCtx.clearRect(0, 0, offscreen.width, offscreen.height);
 }
 
-export function drawFillRect(x, y, w, h, c = "black") {
+export function drawFillRect(x, y, w, h, c = "gray") {
     if (!offscreenCtx) return;
+
     offscreenCtx.fillStyle = c;
     offscreenCtx.fillRect(x, y, w, h);
 }
 
 export function drawStrokeRect(x, y, w, h, l = 2, c = "green") {
     if (!offscreenCtx) return;
+
     offscreenCtx.lineWidth = l;
     offscreenCtx.strokeStyle = c;
     offscreenCtx.strokeRect(x, y, w, h);
 }
 
+export function drawFillCircle(x, y, r, c = "gray") {
+    if (!offscreenCtx) return;
+
+    offscreenCtx.beginPath();
+    offscreenCtx.arc(x, y, r, 0, Math.PI * 2);
+    offscreenCtx.fillStyle = c;
+    offscreenCtx.fill();
+}
+export function drawStrokeCircle(x, y, r, l = 2, c = "green") {
+    if (!offscreenCtx) return;
+
+    offscreenCtx.beginPath();
+    offscreenCtx.arc(x, y, r, 0, Math.PI * 2);
+    offscreenCtx.lineWidth = l;
+    offscreenCtx.strokeStyle = c;
+    offscreenCtx.stroke();
+}
+export function drawText(t, x, y, s = 20, f = "sans-serif", c = "black", a = "left", b = "top") {
+    if (!offscreenCtx) return;
+
+    offscreenCtx.font = `${s}px ${f}`;
+    offscreenCtx.fillStyle = c;
+    offscreenCtx.textAlign = a;
+    offscreenCtx.textBaseline = b;
+    offscreenCtx.fillText(t, x, y);
+}
+
+export async function drawImage(url, x, y, w, h) {
+    if (!offscreenCtx) return;
+
+    const image = new Image();
+    image.src = url;
+
+    await image.decode();
+    offscreenCtx.drawImage(image, x, y, w, h);
+}
+
 export function drawEnd() {
     if (!offscreen || !targetCtx) return;
+
     targetCtx.clearRect(0, 0, targetCtx.canvas.width, targetCtx.canvas.height);
     targetCtx.drawImage(offscreen, 0, 0);
 }
