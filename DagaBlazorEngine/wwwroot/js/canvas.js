69f5d70432a44fa1e54ee078d4e61990319cff12
최민경 <alsruddl1119@naver.com>
2025-06-11 16:51:22 +0900

    canvas 코드 정리

--- DagaBlazorEngine/wwwroot/js/canvas.js
+++ DagaBlazorEngine/wwwroot/js/canvas.js
@@ -1,24 +1,25 @@
 ﻿import { getScreenSize } from "/_content/DagaBlazorEngine/js/module-screensize.js";
 
+let targetCtx;
 let offscreen;
 let offscreenCtx;
 
-export function init() {
-    const { w, h } = getScreenSize();
-    offscreen = new OffscreenCanvas(w, h);
+export function init(canvas) {
+    targetCtx = canvas.getContext("2d");
+    offscreen = new OffscreenCanvas(canvas.width, canvas.height);
     offscreenCtx = offscreen.getContext("2d");
 }
 
-export function drawBegin(canvas) {
-    if (!offscreenCtx || !canvas) return;
+export function drawBegin() {
+    if (!offscreenCtx) return;
 
     const { w, h } = getScreenSize();
-    canvas.width = w;
-    canvas.height = h;
+    targetCtx.canvas.width = w;
+    targetCtx.canvas.height = h;
     offscreen.width = w;
     offscreen.height = h;
 
-    offscreenCtx.clearRect(0, 0, canvas.width, canvas.height);
+    offscreenCtx.clearRect(0, 0, offscreen.width, offscreen.height);
 }
 
 export function drawFillRect(x, y, w, h, c = "black") {
@@ -34,12 +35,8 @@ export function drawStrokeRect(x, y, w, h, l = 2, c = "green") {
     offscreenCtx.strokeRect(x, y, w, h);
 }
 
-export function drawEnd(canvas) {
-    if (!offscreen || !canvas) return;
-
-    const targetCtx = canvas.getContext("2d");
-    if (!targetCtx) return;
-    
-    targetCtx.clearRect(0, 0, canvas.width, canvas.height);
+export function drawEnd() {
+    if (!offscreen || !targetCtx) return;
+    targetCtx.clearRect(0, 0, targetCtx.canvas.width, targetCtx.canvas.height);
     targetCtx.drawImage(offscreen, 0, 0);
 }
