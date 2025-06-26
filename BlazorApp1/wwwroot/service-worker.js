374d508bb9e8e9af8e6b9bf773dbe24bd7af9ce6
최민경 <alsruddl1119@naver.com>
2025-06-26 14:45:45 +0900

    service-worker.js 연구

--- BlazorApp1/wwwroot/service-worker.js
+++ BlazorApp1/wwwroot/service-worker.js
@@ -1,4 +1,14 @@
 // In development, always fetch from the network and do not enable offline support.
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
-self.addEventListener('fetch', () => { });
+self.addEventListener('install', event => {
+    // 초기 캐시 저장
+});
+
+self.addEventListener('activate', event => {
+    // 이전 캐시 정리
+});
+
+self.addEventListener('fetch', event => {
+    // 네트워크 요청 가로채서 캐시 반환
+});
